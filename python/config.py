8971d3a350bbbd3540db6dbf661b2ca4127fad15
Peter Linder <palinder@umich.edu>
2021-05-12 19:17:50 -0400

    Windows-only version as well

---
+++ python/config.py
@@ -0,0 +1,5 @@
+AMAZON_EMAIL = "email@example.com"
+AMAZON_PASSWORD = "password"
+PAYMENT_METHOD = "TD Bank Visa Debit Card"
+CVV = "123" # Credit card 3-digit code
+PRICE_LIMIT = 850 # dollars
---
+++ python/main_windows.py
@@ -0,0 +1,43 @@
+import sys
+import json
+from amazon_buy_bot import *
+import config
+
+def read_in():
+    lines = sys.stdin.readlines()
+    return json.loads(lines[0])
+
+def log(str):
+    print("%s : %s" % (datetime.now().strftime("%Y/%m/%d %H:%M:%S"), str))
+
+def money(number):
+    number = number.strip('$')
+    try:
+        [num,dec]=number.rsplit('.')
+        dec = int(dec)
+        aside = str(dec)
+        x = int('1'+'0'*len(aside))
+        price = float(dec)/x
+        num = num.replace(',','')
+        num = int(num)
+        price = num + price
+    except:
+        price = int(number)
+    return price
+
+def purchase(item_url):
+    amazon.login(password=config.AMAZON_PASSWORD, email=config.AMAZON_EMAIL)
+    amazon.buy(product_url=item_url)
+    amazon.select_payment_method(payment_method=config.PAYMENT_METHOD)
+    amazon.fill_cvv(cvv=config.CVV)
+    amazon.place_order()
+
+def main():
+    data = read_in()
+    brand, series, model, url = data['brand'], data['series'], data['model'], data['url']
+    print('Notification: {} {} series {} at {}'.format(brand, model, series, url))
+    purchase(url)
+
+# Start process
+if __name__ == '__main__':
+    main()
